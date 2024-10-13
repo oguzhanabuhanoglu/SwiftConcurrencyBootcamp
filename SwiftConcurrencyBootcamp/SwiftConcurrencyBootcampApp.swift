8506b5befa3baacaf046fdc521e632e550da8d98
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-10-13 23:06:20 +0300

    First commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Oğuzhan Abuhanoğlu on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
